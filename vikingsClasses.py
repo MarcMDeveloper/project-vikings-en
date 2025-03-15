16773d52988ff9c0bf5b3755c71b9b75562b7294
Marc M <marcmirapeix@gmail.com>
2025-03-15 18:12:13 +0100

    MVP done

--- vikingsClasses.py
+++ vikingsClasses.py
@@ -1,60 +1,84 @@
 import random
 
 # Soldier
-
-
 class Soldier:
     def __init__(self, health, strength):
-        # your code here
-    
+        self.health = health
+        self.strength = strength
+
     def attack(self):
-        # your code here
+        return self.strength
 
     def receiveDamage(self, damage):
-        # your code here
-    
+        self.health -= damage    
 
 # Viking
-
 class Viking(Soldier):
     def __init__(self, name, health, strength):
-        # your code here
+        super().__init__(health, strength)
+        self.name = name
 
     def battleCry(self):
-        # your code here
+        return "Odin Owns You All!"
 
     def receiveDamage(self, damage):
-        # your code here
-
+        self.health -= damage    
+        if self.health > 0:
+            return f"{self.name} has received {damage} points of damage"
+        else:
+            return f"{self.name} has died in act of combat"
+    
 # Saxon
-
 class Saxon(Soldier):
     def __init__(self, health, strength):
-        # your code here
+        super().__init__(health, strength)
 
     def receiveDamage(self, damage):
-        # your code here
+        self.health -= damage    
+        if self.health > 0:
+            return f"A Saxon has received {damage} points of damage"
+        else:
+            return "A Saxon has died in combat"
+        
 
 # WAAAAAAAAAGH
-
 class War():
     def __init__(self):
-        # your code here
+        self.vikingArmy = []
+        self.saxonArmy  = []
 
     def addViking(self, viking):
-        # your code here
+        self.vikingArmy.append(viking)
     
     def addSaxon(self, saxon):
-        # your code here
+        self.saxonArmy.append(saxon)
     
     def vikingAttack(self):
-        # your code here
+        if len(self.saxonArmy) > 0 and len(self.vikingArmy) > 0:
+            output = self.saxonArmy[0].receiveDamage(self.vikingArmy[0].strength)
+            if "died" in output:
+                self.saxonArmy.pop(0)
+        
+            return output
     
     def saxonAttack(self):
-        # your code here
+        if len(self.vikingArmy) > 0 and len(self.saxonArmy) > 0:
+            output = self.vikingArmy[0].receiveDamage(self.saxonArmy[0].strength)
+            if "died" in output:
+                self.vikingArmy.pop(0)
+        
+            return output
 
     def showStatus(self):
-        # your code here
-    pass
+        saxon_quantity = len(self.saxonArmy)
+        viking_quantity = len(self.vikingArmy)
+        
+        if saxon_quantity > 0 and viking_quantity > 0:
+            return "Vikings and Saxons are still in the thick of battle."
+        if viking_quantity > 0:
+            return "Vikings have won the war of the century!"
+        if saxon_quantity:
+            return "Saxons have fought for their lives and survive another day..."
+        
 
 
